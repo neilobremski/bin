7230d55af62918847e7534623292d7b35814957a
Neil C. Obremski <neil@pay-i.com>
2025-12-11 10:59:07 -0800

    git pull all ~/repos

--- install.sh
+++ install.sh
@@ -3,11 +3,22 @@
 # Add the parent directory of this script to PATH
 # Detect if running in zsh or bash
 if [ -n "$ZSH_VERSION" ]; then
-  SCRIPT_DIR="$(cd "$(dirname "${(%):-%x}")" && pwd)"
+  NEIL_BIN="$(cd "$(dirname "${(%):-%x}")" && pwd)"
 else
-  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  NEIL_BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 fi
-echo "Neil's Bin: $SCRIPT_DIR"
 
-export PATH="$SCRIPT_DIR:$PATH"
+# Add Neil's Bin Here to PATH
+echo "Neil's Bin: $NEIL_BIN"
+export PATH="$NEIL_BIN:$PATH"
 
+# If $HOME/repos exists then `git pull` each repository
+if [ -d "$HOME/repos" ]; then
+  for repo in "$HOME/repos"/*; do
+    if [ -d "$repo/.git" ]; then
+      echo "Updating $repo"
+      git -C "$repo" pull &
+    fi
+  done
+  wait  # Wait for all background git pull processes to finish
+fi
